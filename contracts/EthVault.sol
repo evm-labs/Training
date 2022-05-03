b9aeec9d19ae9d518101501f39d71ee738f05fff
"Dimitris Nikolaou" <"dimitrisnikolaou10@gmail.com">
2022-05-03 21:30:14 +0100

    compiling

--- contracts/EthVault.sol
+++ contracts/EthVault.sol
@@ -1,20 +1,17 @@
 pragma solidity 0.8.13;
+// SPDX-License-Identifier: None
 
-/*
-import
-
-*/
 
 contract EthereumVault {
 
-    uint256 intenal constant RECIPIENTAMOUNT = 1 ether;
-    address public payable immutable Owner;
-    mapping (address payable => uint256) addressBalance;
+    uint256 constant internal RECIPIENTAMOUNT = 1 ether;
+    address payable public immutable Owner;
+    mapping (address => uint256) addressBalance;
     address payable[] addresses;
     bool internal locked = false;
 
-    constructor() public {
-        Owner = msg.sender;
+    constructor() {
+        Owner = payable(msg.sender);
     }
 
     modifier onlyOwner(){
@@ -24,16 +21,17 @@ contract EthereumVault {
 
     modifier onlyValidAddresses(address _address){
         require(addressBalance[_address] > 0, "This address does not have the right permissions to enter.");
+        _;
     }
 
-    function addRecipient(address payable _address) external onlyOwner{
-        require(msg.value >= RECIPIENTAMOUNT, "To add a recipient you need to add the required amount.")
+    function addRecipient(address payable _address) external payable onlyOwner{
+        require(msg.value >= RECIPIENTAMOUNT, "To add a recipient you need to add the required amount.");
         addressBalance[_address] = RECIPIENTAMOUNT;
     }
 
     function withdrawMoney(uint256 amountInWei) external onlyValidAddresses(msg.sender){
         require(amountInWei <= addressBalance[msg.sender], "This address does not have the requested amount.");
-        require(!locked, "This method is currently locked.")
+        require(!locked, "This method is currently locked.");
         locked = true;
         (bool success, ) = payable(msg.sender).call{value:amountInWei}(""); 
         require(success, "Transaction failed.");
